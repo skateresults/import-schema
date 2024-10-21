a229a6a33511de02558a8e346b7a889505b8c6d1
Andi Pätzold <github@andipaetzold.com>
2024-10-21 19:14:54 +0200

    feat: add `version`

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -3,6 +3,7 @@ import { athlete } from "./athlete";
 import { ageGroup } from "./ageGroup";
 
 export const schema = z.strictObject({
+  version: z.literal(1),
   athletes: z.array(athlete),
   ageGroups: z.array(ageGroup),
 });
