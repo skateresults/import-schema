da1b6a5f12100437bbc9b88fadbf1162ec1b783a
Andi Pätzold <github@andipaetzold.com>
2025-01-18 01:50:12 +0100

    refactor: split schema and scripts

---
+++ scripts/create-json-schema.ts
@@ -0,0 +1,24 @@
+import {
+  ageGroup,
+  athlete,
+  competition,
+  qualificationRound,
+  raceStatus,
+  root,
+  smallFinal,
+} from "../src";
+import { toJsonSchema } from "@valibot/to-json-schema";
+import { writeFile } from "node:fs/promises";
+
+const jsonSchema = toJsonSchema(root, {
+  definitions: {
+    athlete,
+    raceStatus,
+    smallFinal,
+    qualificationRound,
+    competition,
+    ageGroup,
+  },
+});
+
+await writeFile("schema.json", JSON.stringify(jsonSchema, null, 2));
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,13 @@
-import {
-  ageGroup,
-  athlete,
-  competition,
-  qualificationRound,
-  raceStatus,
-  root,
-  smallFinal,
-} from "./schema";
-import { toJsonSchema } from "@valibot/to-json-schema";
-import { writeFile } from "node:fs/promises";
+import { array, literal, strictObject } from "valibot";
+import { athlete } from "./athlete";
+import { ageGroup } from "./ageGroup";
 
-const jsonSchema = toJsonSchema(root, {
-  definitions: {
-    athlete,
-    raceStatus,
-    smallFinal,
-    qualificationRound,
-    competition,
-    ageGroup,
-  },
+export * from "./athlete";
+export * from "./ageGroup";
+export * from "./competition";
+
+export const root = strictObject({
+  version: literal(1),
+  athletes: array(athlete),
+  ageGroups: array(ageGroup),
 });
-
-await writeFile("schema.json", JSON.stringify(jsonSchema, null, 2));
